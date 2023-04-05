8b47a29da4b86167c867d2c38ddfd116c6e4c865
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-04-05 08:17:51 +0700

    NPM Download Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Test</title>
 </head>
 <body>
-    <p>Daniel Install Berhasil</p>
+    <p class="bg-slate-600 text-center w-full">Daniel Install Berhasil</p>
     <p>Nadya Install Berhasil</p>
 </body>
 </html>
