21f1979fef45dab37bcbbd3ad2001495c5776712
danielzergew <danielzerge.wijaya@gmail.com>
2023-04-14 10:03:49 +0700

    finished signup

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -2,11 +2,17 @@
 
 @section('title', 'Sign Up')
 
-@section('cssextra')
+@section('style')
     <style>
         /* * {
             border: red solid 0.5px;
         } */
+
+        input[type="date"]::-webkit-inner-spin-button,
+        input[type="date"]::-webkit-calendar-picker-indicator {
+            display: none;
+            -webkit-appearance: none;
+        }
     </style>
 @endsection
 
@@ -30,46 +36,48 @@
         <div class="bg-white w-fit h-fit m-auto absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] rounded-3xl flex flex-col justify-center items-center text-center p-5">
             <div class="mb-5">
                 <div class="text-2xl font-bold">Let’s Get Started</div>
-                <div class="text-sm">Create an account to get all LifeBalance features!</div>
+                <div class="text-sm w-[300px]">Create an account to get all LifeBalance features!</div>
             </div>
             <form action="POST" class="flex flex-col gap-2">
-                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4">
+                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2">
                     <span class="material-symbols-outlined">
                         person
                     </span>
                     <input type="text" name="email" id="email" placeholder="Name" class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
                 </div>
-                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4">
+                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2">
                     <span class="material-symbols-outlined">
                         mail
                     </span>
                     <input type="text" name="email" id="email" placeholder="Email" class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
                 </div>
                 <div class="w-[300px] h-[150px] flex items-center text-cDarkGrey justify-between">
-                    <div class="w-[48%] h-full bg-cLightGrey rounded-3xl">
+                    <input type="radio" name="gender" id="male" class="peer/male hidden">
+                    <label class="w-[48%] h-full bg-cLightGrey rounded-3xl peer-checked/male:bg-cBlue duration-300 hover:ring-2" for="male">
                         <div class="w-full h-full bg-cover flex flex-col justify-end items-center p-1" style="background-image: url('assets/male.png')">
                             <div class="text-sm font-bold text-cDarkBlue">Male</div>
                         </div>
-                    </div>
-                    <div class="w-[48%] h-full bg-cLightGrey rounded-3xl">
+                    </label>
+                    <input type="radio" name="gender" id="female" class="peer/female hidden">
+                    <label class="w-[48%] h-full bg-cLightGrey rounded-3xl peer-checked/female:bg-cBlue duration-300 hover:ring-2" for="female">
                         <div class="w-full h-full bg-cover flex flex-col justify-end items-center p-1" style="background-image: url('assets/female.png')">
                             <div class="text-sm font-bold text-cDarkBlue">Female</div>
                         </div>
-                    </div>
+                    </label>
                 </div>
-                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4">
+                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2">
                     <span class="material-symbols-outlined">
                         location_on
                     </span>
                     <input type="text" name="address" id="address" placeholder="Address" class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
                 </div>
-                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4">
+                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2">
                     <span class="material-symbols-outlined">
                         calendar_month
                     </span>
                     <input type="date" name="dob" id="dob" placeholder="DOB (dd/mm/yyyy)" class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
                 </div>
-                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4">
+                <div class="w-[300px] h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2">
                     <span class="material-symbols-outlined">
                         lock
                     </span>
@@ -79,22 +87,24 @@
                     </span>
                 </div>
                 <div class="w-[300px] h-[110px] flex items-center text-cDarkGrey justify-between gap-2">
-                    <div class="h-full aspect-square bg-cLightGrey rounded-3xl p-2 flex flex-col justify-center items-center">
+                    <input type="file" name="photo" id="photo" class="hidden" onchange="loadFile(event)">
+                    <label for="photo" class="h-full aspect-square bg-cLightGrey rounded-3xl p-2 flex flex-col justify-center items-center cursor-pointer duration-300 hover:ring-2">
+                        {{-- <div class="border border-black h-full w-full" id="imgBox"></div> --}}
                         <span class="material-symbols-outlined">
                             image
                         </span>
                         <div class="text-sm">
                             <p>Put your photo here</p>
                         </div>
-                    </div>
+                    </label>
                     <div class="w-full h-full rounded-3xl flex flex-col justify-between">
-                        <div class="w-full h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4">
+                        <div class="w-full h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2">
                             <span class="material-symbols-outlined">
                                 weight
                             </span>
                             <input type="number" name="dob" id="dob" placeholder="Weight (Kg)" class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
                         </div>
-                        <div class="w-full h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4">
+                        <div class="w-full h-[50px] rounded-full bg-cLightGrey flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2">
                             <span class="material-symbols-outlined">
                                 height
                             </span>
@@ -109,8 +119,18 @@
                     </div>
                     <div class="bg-cDarkGrey w-10 h-0.5"></div>
                 </div>
-                <input type="submit" value="Submit" class="w-[300px] h-[50px] rounded-full bg-cBlue text-white hover:bg-white hover:text-cBlue border-2 border-cBlue duration-500 ease-out cursor-pointer text-sm">
+                <input type="submit" value="Submit" class="w-[300px] h-[50px] rounded-full bg-cBlue text-white hover:bg-white hover:text-cBlue border-2 border-cBlue duration-300 ease-out cursor-pointer text-sm">
             </form>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        imgBox = document.getElementById('imgBox');
+
+        var loadFile = function(event) {
+            imgBox.style.backgroundImage = 'url(' + URL.createObjectURL(event.target.files[0]) + ')';
+        }
+    </script>
 @endsection
