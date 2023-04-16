dfe7d3a276417fd34b9d7a3beff51048f4034479
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-04-16 13:38:15 +0700

    Fixing WorkoutDetails

--- resources/views/workout_meditation/workoutDetails.blade.php
+++ resources/views/workout_meditation/workoutDetails.blade.php
@@ -10,33 +10,36 @@
         <div class="h-[70px] flex justify-center items-center text-2xl font-medium">
             <p class="-mr-8">BURN FAT IN 7 DAYS!</p>
         </div>
-        <div class="w-full bg-cBlue h-[270px] py-6 px-10 text-white">
-            <h2 class="text-2xl">BURN FAT IN 7 DAYS!</h2>
-            <p class="text-sm">Workout Program (Results in 1 Week) include ...sadn sadjashdiushdiusahdusdsfsdfdsfdsfdsf</p>
-            <p class="text-sm text-cYellow flex items-center">
-                <span class="material-symbols-outlined inline-block text-cYellow mr-2">
-                    toll
-                </span>
-                35 points
-            </p>
-            {{-- Indicator Container --}}
-            <div class="w-full h-[80px] flex justify-between mt-4 text-black">
-                <div class="w-[90px] h-full bg-white text-center py-2">
-                    <p>Total Time</p>
-                    <p class="font-bold text-2xl text-cBlue">7</p>
-                </div>
-                <div class="w-[90px] h-full bg-white text-center py-2">
-                    <p>Kcal Burn</p>
-                    <p class="font-bold text-2xl text-cBlue">550</p>
-                </div>
-                <div class="w-[90px] h-full bg-white text-center py-2">
-                    <p>Total Pose</p>
-                    <p class="font-bold text-2xl text-cBlue">11</p>
+        <div class="w-full text-white">
+            <div class="h-[270px] bg-cBlue py-6 px-10 ">
+                <h2 class="text-2xl">BURN FAT IN 7 DAYS!</h2>
+                <p class="text-sm">Workout Program (Results in 1 Week) include ...sadn sadjashdiushdiusahdusdsfsdfdsfdsfdsf</p>
+                <p class="text-sm text-cYellow flex items-center">
+                    <span class="material-symbols-outlined inline-block text-cYellow mr-2">
+                        toll
+                    </span>
+                    35 points
+                </p>
+                
+                {{-- Indicator Container --}}
+                <div class="w-full h-[80px] flex justify-between mt-4 text-black">
+                    <div class="w-[90px] h-full bg-white text-center py-2">
+                        <p>Total Time</p>
+                        <p class="font-bold text-2xl text-cBlue">7</p>
+                    </div>
+                    <div class="w-[90px] h-full bg-white text-center py-2">
+                        <p>Kcal Burn</p>
+                        <p class="font-bold text-2xl text-cBlue">550</p>
+                    </div>
+                    <div class="w-[90px] h-full bg-white text-center py-2">
+                        <p>Total Pose</p>
+                        <p class="font-bold text-2xl text-cBlue">11</p>
+                    </div>
                 </div>
             </div>
 
             {{-- Day container --}}
-            <div class="w-full grid grid-cols-2 mt-20 gap-5 font-bold text-4xl">
+            <div class="w-full grid grid-cols-2 gap-5 font-bold text-4xl pt-6 pb-32 px-10">
                 {{-- Day Card --}}
                 <div class="aspect-square bg-white rounded-3xl flex justify-center items-center border-2 text-cBlue border-cBlue">
                     DAY 1
@@ -99,7 +102,8 @@
             </div>
         </div>
 
-        @include('component.navbar')
     </div>
+
+    @include('component.navbar')
     
 @endsection
