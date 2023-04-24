a5ca30964580889d2b81775001be679bc591f90c
Winita-Priyanto <winita.priyanto@binus.ac.id>
2023-04-24 19:41:33 +0700

    Home [Draft]

--- resources/views/home_community/home.blade.php
+++ resources/views/home_community/home.blade.php
@@ -40,27 +40,67 @@
 
     {{-- component.userprogress.blade.php --}}
     <div class="bg-white shadow-lg rounded-3xl h-60">
-        <div class="p-5 flex flex-col text-xl font-bold gap-2 ml-5">
+        <div class="whitespace-break-spaces p-5 flex flex-col text-xl font-bold gap-2 ml-5">
             Your Progress
+
+            ABC TOTAL KCAL
+
+
         </div>
     </div>
 
     {{-- component.featuresbanner.blade.php --}}
-    <div class="mt-3 items-stretch rounded-3xl h-60 bg-transparent mb-3">
-        <div class="p-3 flex flex-col text-2xl font-bold ml-5 bg-black w-40 h-60">
-
-            <div class="p-3 flex flex-col text-2xl font-bold gap-2 ml-5 bg-black w-40 h-60">
-        </div>
+    <div class="bg-white shadow-lg mt-3 items-stretch rounded-3xl h-60 py-3">
+        <div id="default-carousel" class="relative w-full" data-carousel="slide">
+            <!-- Carousel wrapper -->
+            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                <!-- Item 1 -->
+                <div class="duration-700 ease-in-out" data-carousel-item>
+                    <img src="/assets/blue.png" class="absolute block w-40 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                </div>
+                <!-- Item 2 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/assets/green.png" class="absolute block w-40 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                </div>
+                <!-- Item 3 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/assets/orange.png" class="absolute block w-40 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                </div>
+            </div>
+            <!-- Slider indicators -->
+            <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+            </div>
+            <!-- Slider controls -->
+            <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+            <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
         </div>
     </div>
 
     {{-- component.ongoingplan.blade.php --}}
     <ul role="contentinfo" class="py-3">
-        <div class="bg-white shadow-lg rounded-3xl h-60">
-            <div class="p-5 flex flex-col text-xl font-bold gap-2 ml-5">
-                On Going Plan
+        <a href="#" class="flex flex-col items-center bg-white rounded-3xl h-100 shadow-lg md:flex-row hover:bg-cLightGrey dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover rounded-t-lg h-40 m-5 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/assets/eyediet.jpeg" alt="">
+            <div class="flex flex-col justify-between leading-normal">
+                <p class="mb-2 font-normal text-[#FF5768] dark:text-gray-400">Continue your plan.</p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">30 Days Diet Plan for a Healthy Eyes</h5>
+                <div class="w-full mb-5 bg-gray-200 rounded-full dark:bg-gray-700 mb-3">
+                    <div class="bg-[#FF5768] text-xs font-medium text-pink-100 text-center leading-none rounded-full" style="width: 45%"> 45%</div>
+                </div>
             </div>
-        </div>
+        </a>
     </ul>
 
     {{-- component.graph_bodyweight.blade.php --}}
