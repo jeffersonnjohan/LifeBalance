b9c8b142593827032cbdd651a4789d5a502006b1
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-04-26 15:06:50 +0700

    Indicator Box Workout Details Done

--- resources/views/workout_meditation/workoutDetails.blade.php
+++ resources/views/workout_meditation/workoutDetails.blade.php
@@ -23,15 +23,15 @@
                 
                 {{-- Indicator Container --}}
                 <div class="w-full h-[80px] flex justify-between mt-4 text-black">
-                    <div class="w-[90px] h-full bg-white text-center py-2">
+                    <div class="w-[100px] h-full bg-white text-center py-2 rounded-md">
                         <p>Total Time</p>
                         <p class="font-bold text-2xl text-cBlue">7</p>
                     </div>
-                    <div class="w-[90px] h-full bg-white text-center py-2">
+                    <div class="w-[100px] h-full bg-white text-center py-2 rounded-md">
                         <p>Kcal Burn</p>
                         <p class="font-bold text-2xl text-cBlue">550</p>
                     </div>
-                    <div class="w-[90px] h-full bg-white text-center py-2">
+                    <div class="w-[100px] h-full bg-white text-center py-2 rounded-md">
                         <p>Total Pose</p>
                         <p class="font-bold text-2xl text-cBlue">11</p>
                     </div>
