a4b427a9004ef885f998edba6ef7e7fb0c0b6a24
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-05-10 16:04:52 +0700

    Fixing Bug Add More Day & Add more Activity Detail

--- resources/views/adminpage/addWP.blade.php
+++ resources/views/adminpage/addWP.blade.php
@@ -73,12 +73,12 @@
             <div class="px-3 ">
                 <div  class="pt-2 pb-2 ">
                         {{-- Button Add ExerciseDetail --}}
-                        <div class="exerciseDetail w-full h-[50px] rounded-full bg-cBlue bg-opacity-50 flex items-center text-cDarkGrey px-4 gap-2">
+                        <div class="w-full h-[50px] rounded-full bg-cBlue bg-opacity-50 flex items-center text-cDarkGrey px-4 gap-2">
                             <h2 class="border-transparent bg-transparent text-sm font-bold text-cDarkBlue text-center w-full">Day 1</h2>
                             <span class="material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cRed hover:bg-white hover:text-black text-white">
                                 delete
                             </span>
-                            <span class="material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cBlue hover:bg-white hover:text-black text-white">
+                            <span class="exerciseDetail material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cBlue hover:bg-white hover:text-black text-white">
                                 add_circle
                             </span>
                         </div>
@@ -113,55 +113,7 @@
                             delete
                         </span>
                     </div>
-                </div>
-                
-            </div>
-        </div>
-        {{-- Add Details per Day --}}
-        <div id="containerDay">
-            <div class="px-3 ">
-                <div  class="pt-2 pb-2 ">
-                    {{-- Button Add ExerciseDetail --}}
-                    <div class="exerciseDetail w-full h-[50px] rounded-full bg-cBlue bg-opacity-50 flex items-center text-cDarkGrey px-4 gap-2">
-                        <h2 class="border-transparent bg-transparent text-sm font-bold text-cDarkBlue text-center w-full">Day 1</h2>
-                        <span class="material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cRed hover:bg-white hover:text-black text-white">
-                            delete
-                        </span>
-                        <span class="material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cBlue hover:bg-white hover:text-black text-white">
-                            add_circle
-                        </span>
-                    </div>
-                </div>
-                {{-- Add Exercise --}}
-                <div class="containerExercise">
-                    <div  class="flex flex-row gap-2 w-full h-fit pb-2">
-                        <div  class="bg-cDarkGrey bg-opacity-10 h-[140px] w-full rounded-3xl px-4 pt-4">
-                            <div class="w-full h-[50px] rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                                <div class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                                    <select name="exerciseName" id="exerciseName" class="border-0 bg-transparent w-full h-full rounded-full hover:border-transparent focus-within:border-transparent active:border-transparent">
-                                        <option value="exerciseName">Exercise Name</option>
-                                        @foreach ($workoutActivities as $workoutActivity)
-                                            <option value="{{ $workoutActivity->id }}">{{ $workoutActivity->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="flex flex-row justify-between pt-2 w-full h-[px] gap-2">
-                                <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                                    <input type="text" name="repetition" id="repetition" placeholder="Repetition" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                                </div>
-                                <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg ">
-                                    <input type="text" name="calories" id="calories" placeholder="Calories" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                                </div>
-                                <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                                    <input type="text" name="duration" id="duration" placeholder="Duration" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                                </div>
-                            </div>
-                        </div>
-                        <span class=" material-symbols-outlined rounded-full h-fit my-auto p-2 scale-100 duration-300 ease-out bg-cRed hover:bg-white hover:border-cDarkBlue focus:border-5 focus:border-cDarkblue hover:text-black text-white">
-                            delete
-                        </span>
-                    </div>
+                    
                 </div>
                 
             </div>
@@ -194,69 +146,102 @@
 @section('scripts')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script>
-        imgBox = document.getElementById('imgBox');
+        countDay = 1
+
+        // Initialization awal
+        initializationElement()
+
+        // Untuk Preview Image
         var loadFile = function(event) {
             imgBox.style.backgroundImage = 'url(' + URL.createObjectURL(event.target.files[0]) + ')';
         }
 
-        addMoreButton = document.getElementById('addMore');
-        containerDay = document.getElementById('containerDay');
+        // Initilization Element
+        function initializationElement(){
+            imgBox = document.getElementById('imgBox');
+            addMoreButton = document.getElementById('addMore');
+            containerDay = document.getElementById('containerDay');
+            buttonAddExerciseDetail = document.getElementsByClassName('exerciseDetail')
 
-        dayCard = `
-        <div class="px-3">
-            <div  class="pt-2 pb-2 ">
-                <div class="w-full h-[50px] rounded-full bg-cBlue bg-opacity-50 flex items-center text-cDarkGrey px-4 gap-2">
-                    <h2 class="border-transparent bg-transparent text-sm font-bold text-cDarkBlue text-center w-full">Day 1</h2>
-                    <span class="material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cRed hover:bg-white hover:text-black text-white">
-                        delete
-                    </span>
-                    <span class="material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cBlue hover:bg-white hover:text-black text-white">
-                        add_circle
-                    </span>
-                </div>
-            </div>
-            <div  class="flex flex-row gap-2 w-full h-fit pb-2">
-                <div  class="bg-cDarkGrey bg-opacity-10 h-[140px] w-full rounded-3xl px-4 pt-4">
-                    <div class="w-full h-[50px] rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                        <div class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                            <select name="exerciseName" id="exerciseName" class="border-0 bg-transparent w-full h-full rounded-full hover:border-transparent focus-within:border-transparent active:border-transparent">
-                                <option value="exerciseName">Exercise Name</option>
-                                @foreach ($workoutActivities as $workoutActivity)
-                                    <option value="{{ $workoutActivity->id }}">{{ $workoutActivity->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="flex flex-row justify-between pt-2 w-full h-[px] gap-2">
-                        <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                            <input type="text" name="repetition" id="repetition" placeholder="Repetition" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                        </div>
-                        <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg ">
-                            <input type="text" name="calories" id="calories" placeholder="Calories" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                        </div>
-                        <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                            <input type="text" name="duration" id="duration" placeholder="Duration" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                        </div>
-                    </div>
-                </div>
-                <span class=" material-symbols-outlined rounded-full h-fit my-auto p-2 scale-100 duration-300 ease-out bg-cRed hover:bg-white hover:border-cDarkBlue focus:border-5 focus:border-cDarkblue hover:text-black text-white">
-                    delete
-                </span>
-            </div>
-        </div>
+            // Remove event handler
+            for(let i = 0; i < buttonAddExerciseDetail.length; i++){
+                buttonAddExerciseDetail[i] = recreateNode(buttonAddExerciseDetail[i])
+            }
+            containerExercise = document.getElementsByClassName('containerExercise')
+            
+            onClickSetter()
+        }
+
+        // On Click Listener
+        function onClickSetter(){
+            addMoreButton.removeEventListener('click', addMoreButtonOnClick)
+            addMoreButton.addEventListener('click', addMoreButtonOnClick)
     
-        `
-        addMoreButton.addEventListener('click', function(){
-            containerDay.innerHTML += dayCard
-        })
+            // Loop add event listener add exercise detail for each day
+            for(let i = 0; i < buttonAddExerciseDetail.length; i++){
+                buttonAddExerciseDetail[i].addEventListener('click', function(){
+                    addExerciseOnClick(i)
+                })
+            }
+        }
 
-        buttonAddExerciseDetail = document.getElementsByClassName('exerciseDetail')
-        containerExercise = document.getElementsByClassName('containerExercise')
+        // Function Definition
+        function addMoreButtonOnClick(){
+            containerDay.innerHTML += dayCard(++countDay)
+            initializationElement()
+        }
+        
+        function dayCard(i){
+            return `
+            <div class="px-3">
+                <div  class="pt-2 pb-2 ">
+                    <div class="w-full h-[50px] rounded-full bg-cBlue bg-opacity-50 flex items-center text-cDarkGrey px-4 gap-2">
+                        <h2 class="border-transparent bg-transparent text-sm font-bold text-cDarkBlue text-center w-full">Day `+i+`</h2>
+                        <span class="material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cRed hover:bg-white hover:text-black text-white">
+                            delete
+                        </span>
+                        <span class="exerciseDetail material-symbols-outlined rounded-full p-2 scale-100 duration-300 ease-out bg-cBlue hover:bg-white hover:text-black text-white">
+                            add_circle
+                        </span>
+                    </div>
+                </div>
 
-        console.log(containerExercise);
-        // Loop add event listener add exercise detail for each day
-        for(let i = 0; i < buttonAddExerciseDetail.length; i++){
-            buttonAddExerciseDetail[i].addEventListener('click', function(){
+                <div class="containerExercise">
+                    <div  class="flex flex-row gap-2 w-full h-fit pb-2">
+                        <div  class="bg-cDarkGrey bg-opacity-10 h-[140px] w-full rounded-3xl px-4 pt-4">
+                            <div class="w-full h-[50px] rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
+                                <div class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+                                    <select name="exerciseName" id="exerciseName" class="border-0 bg-transparent w-full h-full rounded-full hover:border-transparent focus-within:border-transparent active:border-transparent">
+                                        <option value="exerciseName">Exercise Name</option>
+                                        @foreach ($workoutActivities as $workoutActivity)
+                                            <option value="{{ $workoutActivity->id }}">{{ $workoutActivity->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="flex flex-row justify-between pt-2 w-full h-[px] gap-2">
+                                <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
+                                    <input type="text" name="repetition" id="repetition" placeholder="Repetition" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+                                </div>
+                                <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg ">
+                                    <input type="text" name="calories" id="calories" placeholder="Calories" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+                                </div>
+                                <div class="h-[52px] w-full rounded-full bg-white flex items-center text-cDarkGrey duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
+                                    <input type="text" name="duration" id="duration" placeholder="Duration" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+                                </div>
+                            </div>
+                        </div>
+                        <span class=" material-symbols-outlined rounded-full h-fit my-auto p-2 scale-100 duration-300 ease-out bg-cRed hover:bg-white hover:border-cDarkBlue focus:border-5 focus:border-cDarkblue hover:text-black text-white">
+                            delete
+                        </span>
+                    </div>
+                </div>
+            </div>
+            `
+
+        }
+
+        function addExerciseOnClick(i){
                 containerExercise[i].innerHTML += `
                 {{-- Add Exercise --}}
                 <div  class="flex flex-row gap-2 w-full h-fit pb-2">
@@ -288,7 +273,19 @@
                     </span>
                 </div>
                 `
-            })
+                
+                initializationElement()
+        }
+
+        function recreateNode(el, withChildren) {
+            if (withChildren) {
+                el.parentNode.replaceChild(el.cloneNode(true), el);
+            }
+            else {
+                var newEl = el.cloneNode(false);
+                while (el.hasChildNodes()) newEl.appendChild(el.firstChild);
+                el.parentNode.replaceChild(newEl, el);
+            }
         }
     </script>
 @endsection
