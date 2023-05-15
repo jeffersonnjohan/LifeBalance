a1049df6c56090432afa8e4d3af4ca1093aa1baa
danielzergew <danielzerge.wijaya@gmail.com>
2023-05-15 16:24:43 +0700

    Commit middleware and responsive navbar and some pages

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    public function deleteActiveId(Request $request) {
+    public function logout(Request $request) {
         // ilangin session('activeId')
-        $request->session()->pull('activeId');
+        // $request->session()->pull('activeId');
 
-        if(!session('activeId')) {
-            // kalo sudah hilang
-            return view('login');
-        } else {
-            // kalo masih ada
-            return view('profile');
-        }
+        // if(!session('activeId')) {
+        //     // kalo sudah hilang
+        //     return view('login');
+        // } else {
+        //     // kalo masih ada
+        //     return view('profile');
+        // }
+
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Blade::directive('excerpt', function ($text) {
             return "<?php echo Str::limit($text, 50); ?>";
+        });
+
+        Gate::define('admin', function(User $user) {
+            return $user->is_admin;
         });
     }
 }
