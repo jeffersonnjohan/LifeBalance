f44480751acc615af41770cbb898e6a5f1b0fbb2
Chrystalia <glenysang@gmail.com>
2023-05-16 06:22:39 +0700

    Fixed Diet Pop Up

--- resources/views/diet/diets.blade.php
+++ resources/views/diet/diets.blade.php
@@ -35,7 +35,7 @@
             </div>
         </div>
         <h2 class="text-xl p-2 lg:px-4 font-medium ml-3 mt-2 lg:text-lg">Plan</h2>
-        
+
         <?php $unenroll_plans = array(); $idx = 0; ?>
         <h3 class="flex justify-center text-cGreen">Enrolled Plan</h3>
 
@@ -46,7 +46,7 @@
                 @csrf
                     <input type="hidden" name="diet_id" value="{{ $diet->id }}">
                     <div class="place-items-center grid p-2 lg:w-1/3">
-                    
+
                         <div class="enrolled_element bg-white w-[95%] lg:w-full h-fit lg:h-[150px] place-content-center rounded-3xl p-2 lg:p-4 flex flex-row justify-between shadow-sm group duration-300 ease-out hover:bg-green-200 focus:ring-cGreen" >
                             <div class="flex flex-row items-center">
                                 <div class="rounded-full lg:rounded-3xl bg-cover justify-end items-center h-16 lg:h-32 w-16 lg:w-32 m-2 lg:m-0 border-2 border-cGreen" style="background-image: url('{{ $diet->image . '.png' }}')"></div>
@@ -73,14 +73,14 @@
         <?php $idx = 0;?>
         @if ( $unenroll_plans )
         <h3 class="flex justify-center mt-10 text-cGreen">Not Enrolled Plan</h3>
-        
+
         <div class="lg:flex lg:flex-wrap lg:w-full lg:px-4 ">
             @foreach ($unenroll_plans as $plan)
                 <form action="/dietDays" method="POST" class="unenrolled_form">
                 @csrf
                     <input type="hidden" name="diet_id" value="{{ $plan->id }}">
                     <input type="hidden" name="is_new" value="1">
-                    <div class="place-items-center grid p-2 lg:w-1/3" data-modal-target="popup-modal" data-modal-toggle="popup-modal">
+                    <div class="place-items-center grid p-2 lg:w-1/3" data-modal-target="popup-modal{{ $loop->iteration }}" data-modal-toggle="popup-modal{{ $loop->iteration }}">
                         <div class="bg-white w-[95%] lg:w-full h-fit lg:h-[150px] place-content-center rounded-3xl p-2 lg:p-4 flex flex-row justify-between shadow-sm group duration-300 ease-out hover:bg-green-200 focus:ring-cGreen" >
                             <div class="flex flex-row items-center">
                                 <div class="rounded-full bg-cover justify-end items-center h-16 w-16  m-2 border-2 border-cGreen" style="background-image: url('{{ $plan->image . '.png' }}')"></div>
@@ -98,29 +98,31 @@
                         </div>
                     </div>
                 </form>
-            @endforeach
-           </div>
-        {{-- Pop up --}}
-        <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative w-full max-w-md max-h-full">
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                    <div class="p-6 text-center">
+                {{-- Pop up --}}
+                <div id="popup-modal{{ $loop->iteration }}" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative w-full max-w-md max-h-full">
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal{{ $loop->iteration }}">
+                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                            <div class="p-6 text-center">
 
-                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure to join this plan?</h3>
-                        <button data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure to join this plan?</h3>
+                                <button data-modal-hide="popup-modal{{ $loop->iteration }}" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
 
-                        <button data-modal-hide="popup-modal" type="button" class="popup text-white bg-cGreen hover:bg-cGreen focus:ring-4 focus:outline-none dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                        Yes, I'm sure
-                        </button>
+                                <button data-modal-hide="popup-modal" type="button" class="popup text-white bg-cGreen hover:bg-cGreen focus:ring-4 focus:outline-none dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                Yes, I'm sure
+                                </button>
 
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            @endforeach
+           </div>
+        @endif
+
     </div>
     @include('component.navbar', ['active' => 'diet'])
 
