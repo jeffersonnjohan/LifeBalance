92baac9f376fb7e164454b98299636f2eba3f877
nadyaclrp <nadya.clarine@gmail.com>
2023-06-13 15:35:42 +0700

    Lower the confirmation and delete buttons on the edit plan admin page

--- resources/views/adminpage/addMP.blade.php
+++ resources/views/adminpage/addMP.blade.php
@@ -22,8 +22,11 @@
                     <p class="text-black font-extrabold dark:text-white hover:text-cBlue">Plans</p>
                 </li>
             </ul>
-            <a href="/profile"" class="fixed bg-cBlue rounded-b-3xl flex justify-center items-center aspect-square h-[50px] shadow-lg right-2 -top-0.5 z-10 group duration-300 ease-out hover:bg-white">
-                <div class="bg-white rounded-full p-4 bg-cover mt-2" style="background-image: url({{ '/storage/'. App\Models\User::find(Auth::user()->id)['image'] }})"></div>
+            <a href="/profile""
+                class="fixed bg-cBlue rounded-b-3xl flex justify-center items-center aspect-square h-[50px] shadow-lg right-2 -top-0.5 z-10 group duration-300 ease-out hover:bg-white">
+                <div class="bg-white rounded-full p-4 bg-cover mt-2"
+                    style="background-image: url({{ '/storage/' . App\Models\User::find(Auth::user()->id)['image'] }})">
+                </div>
             </a>
         </div>
     </div>
@@ -34,69 +37,85 @@
     {{-- Page Body Section --}}
     <div class="pt-16  bg-cLightGrey w-full overflow-auto lg:flex lg:flex-row lg:w-full ">
         <form action="/admin/meditation" method="post">
-        @csrf
-        <div class="lg:fixed lg:bg-cBlue lg:flex lg:flex-col lg:place-content-center lg:m-auto lg:h-full lg:rounded-r-[100px] lg:w-[25%]">
-        </div>
-        <div class="lg:flex lg:flex-col lg:w-full lg:ml-[25%]">
-            <div class="px-3">
-                <div class="w-full h-[50px] rounded-full bg-white flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                    <input type="text" name="planTitle" id="planTitle" placeholder="Plan Title" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
-                </div>
+            @csrf
+            <div
+                class="lg:fixed lg:bg-cBlue lg:flex lg:flex-col lg:place-content-center lg:m-auto lg:h-full lg:rounded-r-[100px] lg:w-[25%]">
             </div>
-            <div class="pt-4 px-3">
-                <div class="w-full h-[120px] rounded-3xl bg-white flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
-                    <input type="text" name="description" id="description" placeholder="Description" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+            <div class="lg:flex lg:flex-col lg:w-full lg:ml-[25%]">
+                <div class="px-3">
+                    <div
+                        class="w-full h-[50px] rounded-full bg-white flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
+                        <input type="text" name="planTitle" id="planTitle" placeholder="Plan Title" required
+                            class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+                    </div>
                 </div>
-            </div>
-            <div class="flex-row flex px-3 pb-10">
-                <div class="w-3/6 h-[120px] flex items-center text-cDarkGrey justify-between gap-2 pt-4 p-2 pl-0">
-                    <div class="relative w-full aspect-square h-full bg-white rounded-3xl shadow-lg">
-                        <input type="file" name="image" id="image" accept="image/*,video/*" required class="hidden" onchange="loadFile(event)">
-                        <label for="image" class="h-full w-full aspect-square rounded-3xl p-2 flex flex-col justify-center items-center cursor-pointer duration-300 hover:ring-2 focus-within:ring-2 hover:text-cBlue bg-cover bg-center absolute" id="imgBox">
-                        </label>
-                        <div class="h-full w-full flex flex-col justify-center items-center p-2">
-                            <span class="material-symbols-outlined">
-                                image
-                            </span>
-                            <div class="text-sm text-center">
-                                <p>Input Image/Video</p>
+                <div class="pt-4 px-3">
+                    <div
+                        class="w-full h-[120px] rounded-3xl bg-white flex items-center text-cDarkGrey px-4 duration-300 hover:ring-2 focus-within:text-cBlue focus-within:ring-2 hover:text-cBlue shadow-lg">
+                        <input type="text" name="description" id="description" placeholder="Description" required
+                            class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+                    </div>
+                </div>
+                <div class="flex-row flex px-3 pb-10">
+                    <div class="w-3/6 h-[120px] flex items-center text-cDarkGrey justify-between gap-2 pt-4 p-2 pl-0">
+                        <div class="relative w-full aspect-square h-full bg-white rounded-3xl shadow-lg">
+                            <input type="file" name="image" id="image" accept="image/*,video/*" required
+                                class="hidden" onchange="loadFile(event)">
+                            <label for="image"
+                                class="h-full w-full aspect-square rounded-3xl p-2 flex flex-col justify-center items-center cursor-pointer duration-300 hover:ring-2 focus-within:ring-2 hover:text-cBlue bg-cover bg-center absolute"
+                                id="imgBox">
+                            </label>
+                            <div class="h-full w-full flex flex-col justify-center items-center p-2">
+                                <span class="material-symbols-outlined">
+                                    image
+                                </span>
+                                <div class="text-sm text-center">
+                                    <p>Input Image/Video</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-3/6 h-[120px] items-center text-cDarkGrey justify-between gap-2 pt-4 p-2 pr-0">
+                        <div class="relative aspect-square w-full h-full bg-white rounded-3xl shadow-lg">
+                            <input type="file" name="song" id="song" accept="audio/*" required class="hidden"
+                                onchange="loadFile(event)">
+                            {{-- onchange="loadFile(this.files);"> --}}
+                            <label for="song"
+                                class="h-full w-full aspect-square rounded-3xl p-2 flex flex-col justify-center items-center cursor-pointer duration-300 hover:ring-2 focus-within:ring-2 hover:text-cBlue bg-cover bg-center absolute"
+                                id="imgBox">
+                            </label>
+                            <div class="h-full w-full flex flex-col justify-center items-center p-2">
+                                <span class="material-symbols-outlined">
+                                    library_music
+                                </span>
+                                <div class="text-sm text-center">
+                                    <p>Input Song</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="w-3/6 h-[120px] items-center text-cDarkGrey justify-between gap-2 pt-4 p-2 pr-0">
-                    <div class="relative aspect-square w-full h-full bg-white rounded-3xl shadow-lg">
-                        <input type="file" name="song" id="song" accept="audio/*" required class="hidden" onchange="loadFile(event)">
-                        {{-- onchange="loadFile(this.files);"> --}}
-                        <label for="song" class="h-full w-full aspect-square rounded-3xl p-2 flex flex-col justify-center items-center cursor-pointer duration-300 hover:ring-2 focus-within:ring-2 hover:text-cBlue bg-cover bg-center absolute" id="imgBox">
-                        </label>
-                        <div class="h-full w-full flex flex-col justify-center items-center p-2">
-                            <span class="material-symbols-outlined">
-                                library_music
-                            </span>
-                            <div class="text-sm text-center">
-                                <p>Input Song</p>
-                            </div>
+            </div>
+            {{-- Confirm Button --}}
+            <div class="bottom-0 fixed pb-4 w-full px-3 lg:pb-0 ">
+                <div class="lg:fixed lg:right-0 lg:left-[25%] lg:px-4 lg:pb-2 lg:flex-row lg:flex lg:gap-2 lg:bottom-0">
+                    <div class="pt-2 pb-2 lg:w-[50%]">
+                        <div
+                            class="w-full h-[50px] rounded-full bg-cBlue text-white flex items-center font-bold px-4 duration-300 hover:ring-2 focus-within:text-white focus-within:ring-2 hover:text-cBlue hover:bg-white shadow-lg">
+                            <input type="submit" name="confirmButton" id="confirmButton" value="Confirm" required
+                                class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        {{-- Confirm Button --}}
-        <div class="bottom-0 fixed pb-[80px] w-full px-3 lg:pb-0 ">
-            <div class="lg:fixed lg:right-0 lg:left-[25%] lg:px-4 lg:pb-2 lg:flex-row lg:flex lg:gap-2 lg:bottom-0">
-                <div class="pt-2 pb-2 lg:w-[50%]">
-                    <div class="w-full h-[50px] rounded-full bg-cBlue text-white flex items-center font-bold px-4 duration-300 hover:ring-2 focus-within:text-white focus-within:ring-2 hover:text-cBlue hover:bg-white shadow-lg">
-                        <input type="submit" name="confirmButton" id="confirmButton" value="Confirm" required class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full">
+                    <div class="pt-2 pb-2 bottom-0 lg:w-[50%]">
+                        <a href="/admin/workout"
+                            class="w-full h-[50px] rounded-full bg-cRed text-white flex items-center font-bold px-4 duration-300 hover:ring-2 focus-within:text-white focus-within:ring-2 hover:text-cRed hover:bg-white shadow-lg ring-cRed hover:ring-cRed">
+                            <div
+                                class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full text-center">
+                                Discard</div>
+                        </a>
                     </div>
                 </div>
-                <div class="pt-2 pb-2 bottom-0 lg:w-[50%]">
-                    <a href="/admin/workout" class="w-full h-[50px] rounded-full bg-cRed text-white flex items-center font-bold px-4 duration-300 hover:ring-2 focus-within:text-white focus-within:ring-2 hover:text-cRed hover:bg-white shadow-lg ring-cRed hover:ring-cRed">
-                        <div class="border-transparent bg-transparent focus:ring-0 focus:border-transparent text-sm w-full text-center">Discard</div>
-                    </a>
-                </div>
             </div>
-        </div>
         </form>
     </div>
     {{-- @include('adminpage.adminNavbar', ['active' => 'adminpage.listWorkout']) --}}
@@ -104,10 +123,10 @@
 @endsection
 
 @section('scripts')
-<script>
-    imgBox = document.getElementById('imgBox');
-    var loadFile = function(event) {
-        imgBox.style.backgroundImage = 'url(' + URL.createObjectURL(event.target.files[0]) + ')';
-    }
-</script>
+    <script>
+        imgBox = document.getElementById('imgBox');
+        var loadFile = function(event) {
+            imgBox.style.backgroundImage = 'url(' + URL.createObjectURL(event.target.files[0]) + ')';
+        }
+    </script>
 @endsection
