6eb1e84ce14722209c7a2c6c490a721af1b03d65
Chrystalia <glenysang@gmail.com>
2023-06-15 09:03:47 +0700

    Fixed Diet Back Button Hover Background

--- resources/views/diet/dietdays.blade.php
+++ resources/views/diet/dietdays.blade.php
@@ -9,11 +9,7 @@
 @section('body')
     @extends('component.backbutton')
     @section('backlink', '/diets')
-    {{-- <div  id ='back' class="fixed bg-white rounded-full flex justify-center items-center aspect-square h-[50px] shadow-lg top-2 left-2 z-20 group duration-300 ease-out hover:bg-cBlue">
-        <span class="material-symbols-outlined scale-110  duration-300 ease-out group-hover:text-white">
-            arrow_back
-        </span>
-    </div> --}}
+    @section('hover-bg', 'bg-cGreen')
 
     <?php
         $i = 1;
